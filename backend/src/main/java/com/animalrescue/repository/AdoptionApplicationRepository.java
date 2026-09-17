817946f643b0f189039696abb75860aa46921fa6
SAMRUDDHI SUNIL SHINDE <samruddhishinde2519@gmail.com>
2026-09-17 20:18:04 +0530

    Delete backend directory

--- backend/src/main/java/com/animalrescue/repository/AdoptionApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animalrescue.repository;
-import com.animalrescue.model.AdoptionApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface AdoptionApplicationRepository extends JpaRepository<AdoptionApplication, Long> {
-    List<AdoptionApplication> findByAdopterEmailIgnoreCaseOrderByAppliedAtDesc(String adopterEmail);
-    List<AdoptionApplication> findAllByOrderByAppliedAtDesc();
-    boolean existsByAnimalIdAndAdopterEmailIgnoreCase(Long animalId, String adopterEmail);
-}
--- backend/src/main/java/com/animalrescue/repository/ShelterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animalrescue.repository;
-
-import com.animalrescue.model.Shelter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShelterRepository extends JpaRepository<Shelter, Long> {
-}
